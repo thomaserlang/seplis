7eebfd915b7f47e5cb2bb368668af39f3aa94098
Thomas Erlang <thomas@erlang.dk>
2014-11-08 12:27:06 +0100

    added create test

--- tests/api/handlers/test_play.py
+++ tests/api/handlers/test_play.py
@@ -1,11 +1,21 @@
 # coding=UTF-8
 import json
 import nose
+from seplis.api.testbase import Testbase
 
-class test_play_handler(self):
+class test_play_handler(Testbase):
 
     def test(self):
-        pass
+        self.login(0)
+
+        # create a new play server
+        response = self.post('/1/users/{}/play-servers'.format(self.current_user.id), {
+            'name': 'Thomas',
+            'address': 'http://example.net',
+        })
+        self.assertEqual(response.code, 201, response.body)
+
+
 
 if __name__ == '__main__':
     nose.run(defaultTest=__name__)
