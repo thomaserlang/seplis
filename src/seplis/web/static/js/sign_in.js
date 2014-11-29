4507e45dc112846d34ac56454bc8ae88e3514c86
jonathan1994 <jonathan@erlang.dk>
2014-11-29 15:38:27 +0100

    fixed signin form binding too early

--- src/seplis/web/static/js/sign_in.js
+++ src/seplis/web/static/js/sign_in.js
@@ -1,35 +1,37 @@
-$('#sign-in-form').submit(function(event){
-    event.preventDefault();
-    var btn = $(this).find('button');
-    btn.button('loading')
-    api.post(
-        '/api/sign-in', 
-        $(this).serialize(), 
-        {
-            done: function(data){
-                location.href = $('#next').val();
-            },
-            always: function(){
-                btn.button('reset')                
+$(function(){
+    $('#sign-in-form').submit(function(event){
+        event.preventDefault();
+        var btn = $(this).find('button');
+        btn.button('loading')
+        api.post(
+            '/api/sign-in', 
+            $(this).serialize(), 
+            {
+                done: function(data){
+                    location.href = $('#next').val();
+                },
+                always: function(){
+                    btn.button('reset')                
+                }
             }
-        }
-    );
-});
-
-$('#sign-up-form').submit(function(event){
-    event.preventDefault();
-    var btn = $(this).find('button');
-    btn.button('loading')
-    api.post(
-        '/api/sign-up', 
-        $(this).serialize(), 
-        {
-            done: function(data){
-                location.href = $('#next').val();
-            },
-            always: function(){
-                btn.button('reset')
+        );
+    });
+    
+    $('#sign-up-form').submit(function(event){
+        event.preventDefault();
+        var btn = $(this).find('button');
+        btn.button('loading')
+        api.post(
+            '/api/sign-up', 
+            $(this).serialize(), 
+            {
+                done: function(data){
+                    location.href = $('#next').val();
+                },
+                always: function(){
+                    btn.button('reset')
+                }
             }
-        }
-    );
+        );
+    });
 });
