57105c31529d147d4ce0cff6ed88d62b3c13cf3a
Thomas Erlang <thomas@erlang.dk>
2015-04-03 00:30:38 +0200

    added qtip to static

--- src/seplis/web/static/js/air_dates.js
+++ src/seplis/web/static/js/air_dates.js
@@ -17,4 +17,37 @@ $(function(){
             ).calendar()
         );
     });
+    $('.airdate-show-image').qtip({
+        content: {
+            text: function(event, api) {
+                return $(this).find('.airdate-show-tooltip').html();
+            },
+            button: true,
+        },
+        hide: { 
+            delay: isMobile() ? 0 : 250, 
+            fixed: true,
+            effect: false,
+        },
+        show: {
+            effect: false,
+            delay: isMobile() ? 0 : 500,
+        },
+        position: {
+            adjust: {
+                method: 'flip shift'
+            },
+            viewport: $(window),
+            at: 'center right',
+            my: 'left center',
+        },
+        style: { 
+            classes: 'show-tooltip',
+            def: false,
+            tip: { 
+                width: 15, 
+                offset: -50,
+            },
+        },
+    });
 });
