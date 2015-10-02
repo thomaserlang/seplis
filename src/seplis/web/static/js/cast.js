271cb198303d19def243d92885a88fdfe9f3313a
Thomas Erlang <thomas@erlang.dk>
2015-10-02 20:27:31 +0200

    When connected to a session auto play when chaning to a new episode

--- src/seplis/web/static/js/cast.js
+++ src/seplis/web/static/js/cast.js
@@ -138,6 +138,7 @@
             seplisCast.onLoading();
         seplisCast.onCast((function(d){
             data = d;
+            localStorage.setItem('cast_url', window.location.pathname);
             updateCurrentTime();
             session.sendMessage(
                 namespace, 
@@ -224,6 +225,10 @@
         session = e;
         saveSession(session);
         session.addMessageListener(namespace, receiverMessage);
+        if (window.location.pathname != localStorage.getItem('cast_url')) {
+            play();
+            return;
+        }
         if (session.media.length != 0) {
             console.log('joining session');
             onMediaDiscovered('sessionListener', session.media[0]);
