78be0510c95776bc60801e1b77586b16db11d1fe
Thomas Erlang <thomas@erlang.dk>
2015-11-07 20:16:51 +0100

    Centralized show air dates tool tip

--- src/seplis/web/static/js/show.js
+++ src/seplis/web/static/js/show.js
@@ -86,4 +86,38 @@ $(function(){
     $('.play-next-button').on('touchstart', function(e){
         e.preventDefault();
     });
+
+    $('.show-list-image-tooltip').qtip({
+        content: {
+            text: function(event, api) {
+                return $(this).find('.show-tooltip-text').html();
+            },
+            button: true,
+        },
+        hide: { 
+            delay: isMobile() ? 0 : 250, 
+            fixed: true,
+            effect: false,
+        },
+        show: {
+            effect: false,
+            delay: isMobile() ? 0 : 500,
+        },
+        position: {
+            adjust: {
+                method: 'flip shift'
+            },
+            viewport: $(window),
+            at: 'center right',
+            my: 'left center',
+        },
+        style: { 
+            classes: 'show-tooltip',
+            def: false,
+            tip: { 
+                width: 15, 
+                offset: -50,
+            },
+        },
+    });
 });
