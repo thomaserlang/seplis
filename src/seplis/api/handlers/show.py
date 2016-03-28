8b5543bdeac964efbdd55a3a029b774af5bbafe1
Thomas Erlang <thomas@erlang.dk>
2016-03-28 14:52:49 +0200

    Use query_string instead of bool match for suggest

--- src/seplis/api/handlers/show.py
+++ src/seplis/api/handlers/show.py
@@ -263,18 +263,10 @@ class Handler(base.Handler):
         parts of the word in the `title` or `alternative_titles`.
         """
         return {
-            'bool': {
-                'should': [
-                    {'match': {'title.suggest': {
-                        'query': title, 
-                        'operator': 'and',
-                    }}}, 
-                    {'match': {'alternative_titles.suggest': {
-                        'query': title, 
-                        'operator': 'and',
-                    }}},
-                ],
-            }
+             'query_string': {
+                'fields': ['title.suggest', 'alternative_titles.suggest'],
+                'query': title,
+            },
         }
 
     def append_is_fan(self, shows, user_id=None):
