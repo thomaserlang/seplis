5395bf5e652589bdc4d64eed7baac3d0b2e2f912
Thomas Erlang <thomas@erlang.dk>
2016-04-17 14:56:12 +0200

    Fixed importer worker

--- src/seplis/tasks/update_show.py
+++ src/seplis/tasks/update_show.py
@@ -1,9 +1,4 @@
 
 def update_show(access_token, show_id):
-    from seplis import config
-    from seplis.indexer import Show_indexer
-    indexer = Show_indexer(    
-        url=config['client']['api_url'], 
-        access_token=access_token,
-    )
-    indexer.update_show(show_id)
+    import seplis.importer
+    seplis.importer.shows.update_show_by_id(show_id)
