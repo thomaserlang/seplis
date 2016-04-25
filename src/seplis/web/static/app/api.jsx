d0464733982e42824db80b52c770d75f5d1c9e55
Thomas Erlang <thomas@erlang.dk>
2016-04-26 00:03:37 +0200

    Fixed api getcookie nested in the request method

--- src/seplis/web/static/app/api.jsx
+++ src/seplis/web/static/app/api.jsx
@@ -1,43 +1,42 @@
 import $ from 'jquery';
 
 export function request(url, options = {}) {
-        let query = $.param(options.query || '', true);
-        let method = options.method || (options.data ? 'POST':'GET');
-        let data = options.data;
+    let query = $.param(options.query || '', true);
+    let method = options.method || (options.data ? 'POST':'GET');
+    let data = options.data;
 
-        if (typeof data !== 'undefined' && method !== 'GET') {
-            data = JSON.stringify(data);
-        }
-
-        if (query) {
-            if (url.indexOf('?') === -1) {
-                url += '?' + query;
-            } else {
-                url += '&' + query;
-            }
-        }
-
-        headers = {
-            'Accept': 'application/json; charset=utf-8',
-        }
-        if (method !== 'GET') {
-            headers['X-XSRFToken'] = this.getCookie('_xsrf');
-        }
-
-        return $.ajax({
-            url: url,
-            method: method,
-            data: data,
-            contentType: 'application/json',
-            headers: headers,
-            success: options.success,
-            error: options.error,
-            complete: options.complete,
-        });
+    if (typeof data !== 'undefined' && method !== 'GET') {
+        data = JSON.stringify(data);
     }
 
-    getCookie(name) {
-        var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-        return r ? r[1] : null;
+    if (query) {
+        if (url.indexOf('?') === -1) {
+            url += '?' + query;
+        } else {
+            url += '&' + query;
+        }
     }
+
+    headers = {
+        'Accept': 'application/json; charset=utf-8',
+    }
+    if (method !== 'GET') {
+        headers['X-XSRFToken'] = getCookie('_xsrf');
+    }
+
+    return $.ajax({
+        url: url,
+        method: method,
+        data: data,
+        contentType: 'application/json',
+        headers: headers,
+        success: options.success,
+        error: options.error,
+        complete: options.complete,
+    });
+}
+
+function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : null;
 }
