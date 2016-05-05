41a743625f843f580e23ee2b9cc803748f1b6e6b
Thomas Erlang <thomas@erlang.dk>
2016-05-05 16:10:34 +0200

    Added air dates and next to watch to show view

---
+++ src/seplis/web/static/app/components/shows/episodes/NextToWatchList.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {request} from 'api';
+
+import EpisodeListItem from './EpisodeListItem';
+
+const propTypes = {
+    showId: React.PropTypes.number.isRequired,
+    numberOfEpisodes: React.PropTypes.number,
+}
+
+const defaultProps = {
+    numberOfEpisodes: 3,
+}
+
+class NextToWatchList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            episodes: [],
+        }
+        this.getEpisodes();
+    }
+
+    getEpisodes() {
+        request(
+            `/1/shows/${this.props.showId}/episodes/to-watch`
+        ).success(episode => {
+            request(`/1/shows/${this.props.showId}/episodes`, {
+                query: this.queryFromEpisode(episode),
+            }).success(episodes => {
+                this.setState({episodes:episodes});
+            });
+        }).error(error => {
+            // No more episodes to watch
+            if (error.responseJSON.code === 1301) {
+                this.setState({episodes:[]});
+            }
+        });
+    }
+
+    queryFromEpisode(episode) {
+        let fromNumber = episode.number;
+        return {
+            q: `number:[${fromNumber} TO ${(fromNumber-1) + this.props.numberOfEpisodes}]`,
+            sort: 'number:asc',
+            append: 'user_watched',
+        }
+    }
+
+    render() {
+        return (
+            <span>
+            {this.state.episodes.map((item, key) => (
+                <EpisodeListItem 
+                    showId={this.props.showId}
+                    episode={item}
+                />
+            ))}
+            </span>
+        )
+    }
+}
+NextToWatchList.propTypes = propTypes;
+NextToWatchList.defaultProps = defaultProps;
+
+export default NextToWatchList;
--- src/seplis/web/static/app/views/Show.jsx
+++ src/seplis/web/static/app/views/Show.jsx
@@ -4,6 +4,8 @@ import {isAuthed} from 'utils';
 
 import Loader from 'components/Loader';
 import FanButton from 'components/shows/FanButton';
+import NextToWatchList from 'components/shows/episodes/NextToWatchList';
+import LatestEpisodesSideBar from 'components/shows/episodes/LatestEpisodesSideBar';
 
 const propTypes = {
     showId: React.PropTypes.number.isRequired,
@@ -38,20 +40,40 @@ class Show extends React.Component {
                         <div className="btn-fan__pull-left">
                             <FanButton showId={show.id} isFan={show.is_fan || false} />
                         </div>
-                        <h1>
+                        <h1 className="hidden-xs">
+                            {show.title}
+                            &nbsp;
+                            <small>{show.premiered.substring(0,4)}</small>
+                        </h1>
+                        <h1 className="visible-xs small-device-header">
                             {show.title}
                             &nbsp;
                             <small>{show.premiered.substring(0,4)}</small>
                         </h1>
                     </div> 
 
-                    <div className="col-xs-4 col-sm-4 col-md-4">
-                        <img src={show.poster_image.url + '@SX360'} width="100%" />
+                    <div className="col-xs-4 col-sm-4 col-md-4 col-margin">
+                        <img 
+                            src={show.poster_image.url + '@SX360'} 
+                            width="100%" 
+                        />
                     </div>
-                    <div className="col-xs-8">
-                        <div className="row">
 
-                        </div>
+                    <div className="col-xs-8 col-sm-8 col-md-5 col-margin">
+                        <h2 className="header">
+                            Next to watch
+                        </h2>
+                        <NextToWatchList
+                            showId={this.props.showId}
+                        />
+                    </div>
+                    <div className="col-xs-12 col-md-3 col-margin">
+                        <h2 className="header">
+                            Air dates
+                        </h2>
+                        <LatestEpisodesSideBar
+                            showId={this.props.showId}
+                        />
                     </div>
 
                 </div>
