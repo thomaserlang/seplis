6c95848726a7f997171814702259cbfeb6dc35ce
Thomas Erlang <thomas@erlang.dk>
2016-05-27 15:40:46 +0200

    Added last episode watched

--- src/seplis/api/app.py
+++ src/seplis/api/app.py
@@ -16,6 +16,7 @@ import seplis.api.handlers.air_dates
 import seplis.api.handlers.episode_watched
 import seplis.api.handlers.episode_watching
 import seplis.api.handlers.episode_to_watch
+import seplis.api.handlers.episode_last_watched
 import seplis.api.handlers.etw
 import seplis.api.handlers.user_show_subtitle_lang
 from seplis.api import constants
@@ -159,6 +160,10 @@ class Application(tornado.web.Application):
             URLSpec(
                 r'/1/shows/([0-9]+)/episodes/to-watch', 
                 seplis.api.handlers.episode_to_watch.Handler
+            ),
+            URLSpec(
+                r'/1/shows/([0-9]+)/episodes/last-watched', 
+                seplis.api.handlers.episode_last_watched.Handler
             ),
             URLSpec(
                 r'/1/users/([0-9]+)/etw', 
