ddcde63e442518e8ea736b89aee38e817fcc295a
Thomas Erlang <thomas@erlang.dk>
2016-05-27 19:34:11 +0200

    Added EpisodeLastWatched

---
+++ src/seplis/web/static/app/components/shows/episodes/EpisodeLastWatched.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {request} from 'api'
+import {isAuthed} from 'utils';
+
+import EpisodeListItem from './EpisodeListItem';
+
+const propTypes = {
+    'showId': React.PropTypes.number.isRequired,
+}
+
+class EpisodeLastWatched extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            episode: null,
+        }
+        this.get();
+    }
+
+    get() {
+        if (!isAuthed())
+            return;
+        request(
+            `/1/shows/${this.props.showId}/episodes/last-watched`
+        ).success(episode => {
+            this.setState({episode: episode});
+            console.log(episode);
+        })
+    }
+
+    render() {
+        if (!this.state.episode)
+            return <p className="text-muted">No episodes has been watched</p>;
+        
+        return (
+            <EpisodeListItem 
+                showId={this.props.showId}
+                episode={this.state.episode}
+                displaySeason={true}
+            />
+        );
+    }
+
+}
+EpisodeLastWatched.propTypes = propTypes;
+
+export default EpisodeLastWatched;
