986a10d76d1d138c1c6ea82d949919cc5e80c478
Thomas Erlang <thomas@erlang.dk>
2016-05-29 20:33:26 +0200

    Added show info

--- src/seplis/web/static/app/Routes.jsx
+++ src/seplis/web/static/app/Routes.jsx
@@ -4,15 +4,14 @@ import {Router, Route, browserHistory, IndexRedirect} from 'react-router';
 import Show from './views/show/Show';
 import Main from './views/show/Main';
 import Seasons from './views/show/Seasons';
-
-import SignIn from './views/SignIn';
+import Info from './views/show/Info';
 
 export default (
     <Router history={browserHistory}>
         <Route path="/show/:showId" component={Show}>
             <IndexRedirect to="/show/:showId/main" />
             <Route path="main" component={Main} />
-            <Route path="info" component={SignIn} />
+            <Route path="info" component={Info} />
             <Route path="seasons" component={Seasons} />
         </Route>
     </Router>
