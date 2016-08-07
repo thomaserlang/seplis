2a2ae893cf02f1b2035d41da51d780d33ebd8486
Thomas Erlang <thomas@erlang.dk>
2016-08-07 15:19:59 +0200

    Allow custom classes to show list

--- src/seplis/web/static/app/components/shows/List.jsx
+++ src/seplis/web/static/app/components/shows/List.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 const propTypes = {
     shows: React.PropTypes.array.isRequired,
+    class: React.PropTypes.string,
+}
+
+const defaultTypes = {
+    class: 'col-xs-6 col-sm-4 col-md-3 col-margin'
 }
 
 class List extends React.Component {
 
     renderShow(show) {
         return (
-            <div key={show.id} className="col-xs-6 col-sm-4 col-md-3 col-margin">
+            <div key={show.id} className={this.props.class}>
                 <a href={`/show/${show.id}`}>
                     <img 
                         src={show.poster_image!=null?show.poster_image.url + '@SX269':''} 
@@ -30,5 +35,6 @@ class List extends React.Component {
     }
 }
 List.propTypes = propTypes;
+List.defaultTypes = defaultTypes;
 
 export default List;
