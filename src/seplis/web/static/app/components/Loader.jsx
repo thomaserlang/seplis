776a1ad674cae4f36387ef7936ab8cf82f974f85
Thomas Erlang <thomas@erlang.dk>
2017-04-29 13:41:06 +0200

    Added horizontal center option to the loader

--- src/seplis/web/static/app/components/Loader.jsx
+++ src/seplis/web/static/app/components/Loader.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ClassNames from 'classnames';
 
 import './Loader.scss';
 
+const propTypes = {
+    hcenter: PropTypes.boolean,
+}
+
+const defaultProps = {
+    hcenter: false,
+}
+
 class Loader extends React.Component {
 
-    render() {
+    render() {        
+        let cls = ClassNames({
+            loader: true,
+            'loader-hcenter': this.props.hcenter,
+        });
         return (
-            <div className="loader"></div>
+            <div className={cls}></div>
         )
     }
 
 }
+Loader.propTypes = propTypes;
+Loader.defaultProps = defaultProps;
 
 export default Loader;
