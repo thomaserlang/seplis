4653347841e1a97ecaffaeb0b4c9fd4a3bc8ce1a
Thomas Erlang <thomas@erlang.dk>
2017-11-28 20:40:31 +0100

    Better buffer values for hls.js

--- src/seplis/web/static/app/components/player/Player.jsx
+++ src/seplis/web/static/app/components/player/Player.jsx
@@ -118,6 +118,8 @@ class Player extends React.Component {
             manifestLoadingTimeOut: 30000,
             maxSeekHole: 12,
             debug: false,
+            maxMaxBufferLength: 300,
+            maxBufferLength: 60,
         });
         this.hls.loadSource(url);
         this.hls.attachMedia(this.video);
