3b9dcb7daeaa3cbc5dd44dda5417f4138643fd98
Thomas Erlang <thomas@erlang.dk>
2018-03-07 22:46:01 +0100

    Supported browsers

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,8 @@ module.exports = {
         require('autoprefixer')({
             cascade : false,
             browsers: [
-                "Android 2.3",
-                "Android >= 4",
-                "Chrome >= 20",
-                "Firefox >= 24",
-                "Explorer >= 8",
-                "iOS >= 6",
-                "Opera >= 12",
-                "Safari >= 6"
+                "> 1%",
+                "last 2 versions",
             ],
        })
     ]
