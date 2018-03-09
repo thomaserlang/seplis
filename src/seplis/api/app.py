1644ab0bd8bc2fde9d3162884ccaea4bfc4b3079
Thomas Erlang <thomas@erlang.dk>
2018-03-09 22:11:46 +0100

    More link organization

--- src/seplis/api/app.py
+++ src/seplis/api/app.py
@@ -20,52 +20,42 @@ class Application(web.Application):
         static_path = os.path.join(os.path.dirname(__file__), 'static')
         urls = [
             U(r'/(favicon.ico)', web.StaticFileHandler, {'path': os.path.join(static_path, 'favicon.ico')}),
-            U( r'/static/(.*)', web.StaticFileHandler, {'path': static_path}),
+            U(r'/static/(.*)', web.StaticFileHandler, {'path': static_path}),
 
             U(r'/1/shows', h.show.Handler),
             U(r'/1/shows/externals/([a-z_-]+)/([a-z0-9]+)', h.show.External_handler),            
             U(r'/1/shows/([0-9]+)', h.show.Handler),
             U(r'/1/shows/([0-9,]+)', h.show.Multi_handler),
             U(r'/1/shows/([0-9]+)/user-stats', h.user_show_stats.Handler),
-            U(r'/1/shows/([0-9]+)/episodes', h.episode.Handler),
-            U(r'/1/shows/([0-9]+)/episodes/([0-9]+)', h.episode.Handler),
-            U(r'/1/shows/([0-9]+)/episodes/([0-9]+)/play-servers', h.episode.Play_servers_handler),
             U(r'/1/shows/([0-9]+)/images', h.image.Handler, {'relation_type': 'show'}),
             U(r'/1/shows/([0-9]+)/images/([0-9]+)', h.image.Handler),
             U(r'/1/shows/([0-9]+)/images/([0-9]+)/data', h.image.Data_handler),
-
             U(r'/1/shows/([0-9]+)/update', h.show.Update_handler),
-
-            U(r'/1/users', h.user.Collection_handler),
-            U(r'/1/users/current', h.user.Current_handler),            
-            U(r'/1/users/current/change-password', h.user.Change_password_handler),
-            
-            U(r'/1/users/([0-9]+)', h.user.Handler),
-        
-            U(r'/1/users/([0-9]+)/fan-of', h.user_fan_of.Handler),
-            U(r'/1/users/([0-9]+)/fan-of/([0-9]+)', h.user_fan_of.Handler),
-            U(r'/1/users/([0-9]+)/show-stats', h.user_shows_stats.Handler),
-
-            U(r'/1/users/([0-9]+)/air-dates', h.air_dates.Handler),
-
-            U(r'/1/users/([0-9]+)/shows-watched', h.shows_watched.Handler,),      
-
-            U(r'/1/users/([0-9]+)/shows-recently-aired', h.shows_recently_aired.Handler,),           
-
-            U(r'/1/users/([0-9]+)/shows-countdown', h.shows_countdown.Handler),
-            U(r'/1/users/([0-9]+)/shows-etw', h.shows_etw.Handler),
-        
+            U(r'/1/shows/([0-9]+)/episodes', h.episode.Handler),
+            U(r'/1/shows/([0-9]+)/episodes/to-watch', h.episode_to_watch.Handler),
+            U(r'/1/shows/([0-9]+)/episodes/last-watched', h.episode_last_watched.Handler),
+            U(r'/1/shows/([0-9]+)/episodes/([0-9]+)', h.episode.Handler),
+            U(r'/1/shows/([0-9]+)/episodes/([0-9]+)/play-servers', h.episode.Play_servers_handler),
             U(r'/1/shows/([0-9]+)/episodes/([0-9]+)/watched', h.episode_watched.Handler),
             U(r'/1/shows/([0-9]+)/episodes/([0-9]+)-([0-9]+)/watched', h.episode_watched.Range_handler),
             U(r'/1/shows/([0-9]+)/episodes/([0-9]+)/position', h.episode_position.Handler),
-            U(r'/1/shows/([0-9]+)/episodes/to-watch', h.episode_to_watch.Handler),
-            U(r'/1/shows/([0-9]+)/episodes/last-watched', h.episode_last_watched.Handler),
-
+            
+            U(r'/1/users', h.user.Collection_handler),
+            U(r'/1/users/current', h.user.Current_handler),            
+            U(r'/1/users/current/change-password', h.user.Change_password_handler),
+            U(r'/1/users/([0-9]+)', h.user.Handler),        
+            U(r'/1/users/([0-9]+)/fan-of', h.user_fan_of.Handler),
+            U(r'/1/users/([0-9]+)/fan-of/([0-9]+)', h.user_fan_of.Handler),
+            U(r'/1/users/([0-9]+)/show-stats', h.user_shows_stats.Handler),
+            U(r'/1/users/([0-9]+)/air-dates', h.air_dates.Handler),
+            U(r'/1/users/([0-9]+)/shows-watched', h.shows_watched.Handler,),
+            U(r'/1/users/([0-9]+)/shows-recently-aired', h.shows_recently_aired.Handler,),           
+            U(r'/1/users/([0-9]+)/shows-countdown', h.shows_countdown.Handler),
+            U(r'/1/users/([0-9]+)/shows-etw', h.shows_etw.Handler),        
             U(r'/1/users/([0-9]+)/play-servers/([0-9]+)', h.play.Server_handler),
             U(r'/1/users/([0-9]+)/play-servers', h.play.Server_handler),       
             U(r'/1/users/([0-9]+)/play-servers/([0-9]+)/users', h.play.Access_handler),     
             U(r'/1/users/([0-9]+)/play-servers/([0-9]+)/users/([0-9]+)', h.play.Access_handler),
-           
             U(r'/1/users/([0-9]+)/subtitle-lang/shows/([0-9]+)', h.user_show_subtitle_lang.Handler),
 
             U(r'/1/apps', h.app.Handler),
