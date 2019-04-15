a9022e6604a6a5fc39cbdad772223ae82e618463
Thomas Erlang <thomas@erlang.dk>
2019-04-15 16:25:32 +0200

    Version 0.1.52

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='seplis',
-    version='0.1.51',
+    version='0.1.52',
     author='Thomas Erlang',
     author_email='thomas@erlang.dk',
     url='https://github.com/thomaserlang/seplis',
