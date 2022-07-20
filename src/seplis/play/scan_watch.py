d63d6ab36400a6af48f81397dbb522e68b45884e
Thomas Erlang <thomas@erlang.dk>
2022-07-20 18:38:05 +0200

    Fix scan watch

--- src/seplis/play/scan_watch.py
+++ src/seplis/play/scan_watch.py
@@ -28,7 +28,7 @@ class Handler(PatternMatchingEventHandler):
         path = event.src_path
         if event.event_type == 'moved':
             path = event.dest_path
-        return (scan.parse(path), path)
+        return (self.scanner.parse(path), path)
             
     def update(self, event):
         try:
