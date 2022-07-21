ce2af5e93f025e9b3d74f7c5fe5a176faccec07c
Thomas Erlang <thomas@erlang.dk>
2022-07-21 02:21:23 +0200

    Fixed movie watched returning stalled data

--- src/seplis/api/handlers/movie_watched.py
+++ src/seplis/api/handlers/movie_watched.py
@@ -31,85 +31,83 @@ class Handler(base.Handler):
     async def post(self, movie_id: str):
         data: Watched_schema = self.validate()
         async with self.async_session() as session:
-            async with session.begin():
-                sql = insert(models.Movie_watched).values(
-                    movie_id=movie_id,
-                    user_id=self.current_user.id,
-                    watched_at=data.watched_at,
-                    times=1
-                )
-                sql = sql.on_duplicate_key_update(
-                    watched_at=sql.inserted.watched_at,
-                    times=models.Movie_watched.times + 1,
-                    position=0,
-                )
-                sql_history = insert(models.Movie_watched_history).values(
-                    movie_id=movie_id,
-                    user_id=self.current_user.id,
-                    watched_at=data.watched_at,
-                )
-                await asyncio.gather(
-                    session.execute(sql),
-                    session.execute(sql_history),
-                )
-                r = await self.get_watched(session, movie_id)
-                await session.commit()                
-                self.write_object(r)
+            sql = insert(models.Movie_watched).values(
+                movie_id=movie_id,
+                user_id=self.current_user.id,
+                watched_at=data.watched_at,
+                times=1
+            )
+            sql = sql.on_duplicate_key_update(
+                watched_at=sql.inserted.watched_at,
+                times=models.Movie_watched.times + 1,
+                position=0,
+            )
+            sql_history = insert(models.Movie_watched_history).values(
+                movie_id=movie_id,
+                user_id=self.current_user.id,
+                watched_at=data.watched_at,
+            )
+            await asyncio.gather(
+                session.execute(sql),
+                session.execute(sql_history),
+            )
+            await session.commit()
+            r = await self.get_watched(session, movie_id)
+            self.write_object(r)
 
     @authenticated(constants.LEVEL_PROGRESS)
     async def delete(self, movie_id: str):
         async with self.async_session() as session:
-            async with session.begin():
-                w = await self.get_watched(session, movie_id)
-                if w.times <= 1:
-                    await asyncio.gather(
-                        session.execute(delete(models.Movie_watched).where(
-                            models.Movie_watched.movie_id == movie_id,
-                            models.Movie_watched.user_id == self.current_user.id,
-                        )),
-                        session.execute(delete(models.Movie_watched_history).where(
-                            models.Movie_watched_history.movie_id == movie_id,
-                            models.Movie_watched_history.user_id == self.current_user.id,
-                        )),
-                    )
-                    await session.commit()
-                    self.set_status(204)
-                else:
-                    watched_at = await session.scalar(select(models.Movie_watched_history.watched_at).where(
+            w = await self.get_watched(session, movie_id)
+            if w.times <= 1:
+                await asyncio.gather(
+                    session.execute(delete(models.Movie_watched).where(
+                        models.Movie_watched.movie_id == movie_id,
+                        models.Movie_watched.user_id == self.current_user.id,
+                    )),
+                    session.execute(delete(models.Movie_watched_history).where(
                         models.Movie_watched_history.movie_id == movie_id,
                         models.Movie_watched_history.user_id == self.current_user.id,
+                    )),
+                )
+                await session.commit()
+                self.set_status(204)
+            else:
+                watched_at = await session.scalar(select(models.Movie_watched_history.watched_at).where(
+                    models.Movie_watched_history.movie_id == movie_id,
+                    models.Movie_watched_history.user_id == self.current_user.id,
+                ).limit(1))
+                if w.position > 0:
+                    await session.execute(update(models.Movie_watched).where(
+                        models.Movie_watched.movie_id == movie_id,
+                        models.Movie_watched.user_id == self.current_user.id,
+                    ).values(
+                        position=0,
+                        watched_at=watched_at,
+                    ))
+                else:
+                    id_ = await session.scalar(select(models.Movie_watched_history.id).where(
+                        models.Movie_watched_history.movie_id == movie_id,
+                        models.Movie_watched_history.user_id == self.current_user.id,
+                    ).order_by(
+                        models.Movie_watched_history.watched_at.desc()
                     ).limit(1))
-                    if w.position > 0:
-                        await session.execute(update(models.Movie_watched).where(
+                    await asyncio.gather(
+                        session.execute(update(models.Movie_watched).where(
                             models.Movie_watched.movie_id == movie_id,
                             models.Movie_watched.user_id == self.current_user.id,
                         ).values(
+                            times=models.Movie_watched.times - 1,
                             position=0,
                             watched_at=watched_at,
+                        )),
+                        session.execute(delete(models.Movie_watched_history).where(
+                            models.Movie_watched_history.id == id_,
                         ))
-                    else:
-                        id_ = await session.scalar(select(models.Movie_watched_history.id).where(
-                            models.Movie_watched_history.movie_id == movie_id,
-                            models.Movie_watched_history.user_id == self.current_user.id,
-                        ).order_by(
-                            models.Movie_watched_history.watched_at.desc()
-                        ).limit(1))
-                        await asyncio.gather(
-                            session.execute(update(models.Movie_watched).where(
-                                models.Movie_watched.movie_id == movie_id,
-                                models.Movie_watched.user_id == self.current_user.id,
-                            ).values(
-                                times=models.Movie_watched.times - 1,
-                                position=0,
-                                watched_at=watched_at,
-                            )),
-                            session.execute(delete(models.Movie_watched_history).where(
-                                models.Movie_watched_history.id == id_,
-                            ))
-                        )
-                    r = await self.get_watched(session, movie_id)
-                    await session.commit()
-                    self.write_object(r)
+                    )
+                await session.commit()
+                r = await self.get_watched(session, movie_id)
+                self.write_object(r)
 
     async def get_watched(self, session, movie_id: str):
         r = await session.execute(select(
