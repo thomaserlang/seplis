5cde979c8916b5de09ed49ce15927af000f057d6
Thomas Erlang <thomas@erlang.dk>
2022-07-26 23:57:43 +0200

    It's just files

--- src/seplis/play/handlers/transcoders/hls.py
+++ src/seplis/play/handlers/transcoders/hls.py
@@ -24,7 +24,7 @@ class Hls_transcoder(base.Transcoder):
         return 'media.m3u8'
 
     async def wait_for_media(self) -> bool:
-        ts_files = 0
+        files = 0
 
         while True:
             await asyncio.sleep(0.5)
@@ -32,6 +32,6 @@ class Hls_transcoder(base.Transcoder):
                 async with async_open(self.media_path, "r") as afp:
                     async for line in afp:                        
                         if not '#' in line:
-                            ts_files += 1   
-            if ts_files >= 1:
+                            files += 1   
+            if files >= 1:
                 return True
