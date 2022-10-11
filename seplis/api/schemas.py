aa4ef1360c0732827bf546c4f958d32b5c98a861
Thomas Erlang <thomas@erlang.dk>
2022-10-11 10:23:36 +0200

    Fixed movie schema

--- seplis/api/schemas.py
+++ seplis/api/schemas.py
@@ -27,8 +27,8 @@ class Movie_schema(BaseModel):
     externals: Optional[Movie_externals_schema]
     status: Optional[conint(ge=0, le=5)] # Status: 0: Unknown, 1: Released, 2: Rumored, 3: Planned, 4: In production, 5: Post production, 6: Canceled,
     language: Optional[constr(min_length=1, max_length=20)]
-    poster_image_id: Optional[conint(gt=1)]
-    runtime: Optional[conint(gt=0)]
+    poster_image_id: Optional[conint(ge=1)]
+    runtime: Optional[conint(ge=0)]
     release_date: Optional[date]
 
     class Config:
