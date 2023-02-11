46fab4a3a4ef073018998ff8c20bfcd823707e69
Thomas Erlang <thomas@erlang.dk>
2023-02-11 02:46:37 +0100

    Fixed following button returning undefined if not authenticated

--- seplis/web/ui/components/series/following-button.tsx
+++ seplis/web/ui/components/series/following-button.tsx
@@ -1,7 +1,6 @@
 import { StarIcon } from '@chakra-ui/icons'
 import { Button, useToast } from '@chakra-ui/react'
 import api from '@seplis/api'
-import { IMovieStared } from '@seplis/interfaces/movie'
 import { isAuthed } from '@seplis/utils'
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
@@ -10,14 +9,14 @@ import { useFocusable } from '@noriginmedia/norigin-spatial-navigation'
 import { focusedBorder } from '@seplis/styles'
 import { ISeriesFollowing } from '@seplis/interfaces/series'
 
-export default function StaredButton({ seriesId }: { seriesId: number}) {
+export default function StaredButton({ seriesId }: { seriesId: number }) {
     const toast = useToast()
     const [loading, setLoading] = useState(false)
     const [following, setFollowing] = useState(false)
 
     const { isInitialLoading } = useQuery(['series', 'following-button', seriesId], async () => {
         if (!isAuthed())
-            return        
+            return null
         const result = await api.get<ISeriesFollowing>(`/2/series/${seriesId}/following`)
         setFollowing(result.data.following)
         return result.data
@@ -32,14 +31,14 @@ export default function StaredButton({ seriesId }: { seriesId: number}) {
                 await api.put(`/2/series/${seriesId}/following`)
                 setFollowing(true)
             }
-        } catch (e) { 
+        } catch (e) {
             toast({
                 title: ErrorMessageFromResponse(e),
                 status: 'error',
                 isClosable: true,
                 position: 'top',
             })
-            
+
         } finally {
             setLoading(false)
         }
@@ -49,15 +48,15 @@ export default function StaredButton({ seriesId }: { seriesId: number}) {
     })
 
 
-    return <Button 
+    return <Button
         ref={ref}
-        isLoading={isInitialLoading || loading} 
-        colorScheme={'green'} 
-        variant={following?'solid':'outline'}
+        isLoading={isInitialLoading || loading}
+        colorScheme={'green'}
+        variant={following ? 'solid' : 'outline'}
         onClick={handleClick}
         leftIcon={<StarIcon />}
-        style={focused?focusedBorder:null}
+        style={focused ? focusedBorder : null}
     >
-        {following?'Following':'Follow'}
+        {following ? 'Following' : 'Follow'}
     </Button>
 }
