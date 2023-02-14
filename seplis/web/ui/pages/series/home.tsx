293546a70573ec078766a78648c633e587fd53ab
Thomas Erlang <thomas@erlang.dk>
2023-02-14 01:36:54 +0100

    Changed series home to open the series in a modal

--- seplis/web/ui/pages/series/home.tsx
+++ seplis/web/ui/pages/series/home.tsx
@@ -1,15 +1,17 @@
-import { Stack } from '@chakra-ui/react'
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Stack, useDisclosure } from '@chakra-ui/react'
 import { FocusContext, useFocusable } from '@noriginmedia/norigin-spatial-navigation'
 import MainMenu from '@seplis/components/main-menu'
+import Series from '@seplis/components/series/series'
 import Slider from '@seplis/components/slider'
-import { ISeriesAndEpisode, ISeriesUser } from '@seplis/interfaces/series'
+import { ISeries, ISeriesAndEpisode, ISeriesUser } from '@seplis/interfaces/series'
 import { dateCountdown, episodeNumber, setTitle } from '@seplis/utils'
-import { useCallback, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useCallback, useEffect, useState } from 'react'
+
 
 export default function SeriesHome() {
     const { ref, focusKey, focusSelf } = useFocusable()
-    const navigate = useNavigate()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [series, setSeries] = useState<ISeries>(null)
 
     useEffect(() => {
         setTitle('Series Home')
@@ -26,81 +28,89 @@ export default function SeriesHome() {
         });
     }, [ref])
 
-    return <FocusContext.Provider value={focusKey}>
-        <MainMenu />
+    const itemSelected = (item: ISeriesUser | ISeriesAndEpisode) => {
+        setSeries(item.series)
+        onOpen()
+    }
 
-        <Stack ref={ref} marginTop="0.5rem">
+    return <>
+        <FocusContext.Provider value={focusKey}>
+            <MainMenu />
 
-            <Slider<ISeriesUser>
-                title="Watched"
-                url="/2/users/me/series-watched"
-                parseItem={(item) => (
-                    {
-                        key: `series-${item.series.id}`,
-                        title: item.series.title,
-                        img: item.series.poster_image?.url,
-                        bottomText: episodeNumber(item.last_episode_watched),
-                    }
-                )}
-                onFocus={onRowFocus}
-                onItemSelected={(item) => {
-                    navigate(`/series/${item.series.id}`)
-                }}
-            />
+            <Stack ref={ref} marginTop="0.5rem">
 
-            <Slider<ISeriesAndEpisode>
-                title="Recently Aired"
-                url="/2/users/me/series-recently-aired"
-                parseItem={(item) => (
-                    {
-                        key: `series-${item.series.id}`,
-                        title: item.series.title,
-                        img: item.series.poster_image?.url,
-                        bottomText: episodeNumber(item.episode),
-                    }
-                )}
-                onFocus={onRowFocus}
-                onItemSelected={(item) => {
-                    navigate(`/series/${item.series.id}`)
-                }}
-            />
+                <Slider<ISeriesUser>
+                    title="Watched"
+                    url="/2/users/me/series-watched"
+                    parseItem={(item) => (
+                        {
+                            key: `series-${item.series.id}`,
+                            title: item.series.title,
+                            img: item.series.poster_image?.url,
+                            bottomText: episodeNumber(item.last_episode_watched),
+                        }
+                    )}
+                    onFocus={onRowFocus}
+                    onItemSelected={itemSelected}
+                />
 
-            <Slider<ISeriesAndEpisode>
-                title="Countdown"
-                url="/2/users/me/series-countdown"
-                parseItem={(item) => (
-                    {
-                        key: `series-${item.series.id}`,
-                        title: item.series.title,
-                        img: item.series.poster_image?.url,
-                        bottomText: episodeNumber(item.episode),
-                        topText: dateCountdown(item.episode.air_datetime),
-                    }
-                )}
-                onFocus={onRowFocus}
-                onItemSelected={(item) => {
-                    navigate(`/series/${item.series.id}`)
-                }}
-            />
+                <Slider<ISeriesAndEpisode>
+                    title="Recently Aired"
+                    url="/2/users/me/series-recently-aired"
+                    parseItem={(item) => (
+                        {
+                            key: `series-${item.series.id}`,
+                            title: item.series.title,
+                            img: item.series.poster_image?.url,
+                            bottomText: episodeNumber(item.episode),
+                        }
+                    )}
+                    onFocus={onRowFocus}
+                    onItemSelected={itemSelected}
+                />
 
-            <Slider<ISeriesAndEpisode>
-                title="Episodes to Watch"
-                url="/2/users/me/series-to-watch"
-                parseItem={(item) => (
-                    {
-                        key: `series-${item.series.id}`,
-                        title: item.series.title,
-                        img: item.series.poster_image?.url,
-                        bottomText: episodeNumber(item.episode),
-                    }
-                )}
-                onFocus={onRowFocus}
-                onItemSelected={(item) => {
-                    navigate(`/series/${item.series.id}`)
-                }}
-            />
+                <Slider<ISeriesAndEpisode>
+                    title="Countdown"
+                    url="/2/users/me/series-countdown"
+                    parseItem={(item) => (
+                        {
+                            key: `series-${item.series.id}`,
+                            title: item.series.title,
+                            img: item.series.poster_image?.url,
+                            bottomText: episodeNumber(item.episode),
+                            topText: dateCountdown(item.episode.air_datetime),
+                        }
+                    )}
+                    onFocus={onRowFocus}
+                    onItemSelected={itemSelected}
+                />
 
-        </Stack>
-    </FocusContext.Provider>
+                <Slider<ISeriesAndEpisode>
+                    title="Episodes to Watch"
+                    url="/2/users/me/series-to-watch"
+                    parseItem={(item) => (
+                        {
+                            key: `series-${item.series.id}`,
+                            title: item.series.title,
+                            img: item.series.poster_image?.url,
+                            bottomText: episodeNumber(item.episode),
+                        }
+                    )}
+                    onFocus={onRowFocus}
+                    onItemSelected={itemSelected}
+                />
+
+            </Stack>
+        </FocusContext.Provider>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent maxWidth="1100px" backgroundColor="gray.900" padding="1rem 0">
+                <ModalCloseButton />
+                <ModalBody>
+                    <Series series={series} />
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    </>
 
 }
