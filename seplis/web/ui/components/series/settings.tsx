3758731bab757d7e6f4407097cab85b2a45b64ab
Thomas Erlang <thomas@erlang.dk>
2023-02-17 22:54:08 +0100

    Added imports to series settings

--- seplis/web/ui/components/series/settings.tsx
+++ seplis/web/ui/components/series/settings.tsx
@@ -1,73 +1,99 @@
-import { Alert, AlertIcon, AlertTitle, Box, Button, FormControl, FormLabel, Heading, Input, Stack, useToast } from '@chakra-ui/react'
+import { Alert, AlertIcon, AlertTitle, Button, Flex, FormControl, FormLabel, Heading, Input, Select, useToast } from '@chakra-ui/react'
 import api from '@seplis/api'
-import { ISeries } from '@seplis/interfaces/series'
+import { ISeries, ISeriesImporters } from '@seplis/interfaces/series'
 import { TExternals } from '@seplis/interfaces/types'
+import { useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { ErrorMessageFromResponse } from '../error'
 
 interface IData {
     externals: TExternals
+    importers: ISeriesImporters
 }
 
-export default function Settings({ series }: { series: ISeries }) {    
+export default function Settings({ series }: { series: ISeries }) {
     const { handleSubmit, register, formState: { isSubmitting } } = useForm<IData>({
         defaultValues: {
-            externals: {...series.externals}
+            externals: { ...series.externals },
+            importers: { ...series.importers },
         }
     })
-    const [ error, setError ] = useState<JSX.Element>(null)
+    const [error, setError] = useState<JSX.Element>(null)
     const toast = useToast()
+    const queryClient = useQueryClient()
 
     const onSubmit = handleSubmit(async (data) => {
         try {
             setError(null)
-            await api.patch<ISeries>(`/2/series/${series.id}`, data)
+            const result = await api.patch<ISeries>(`/2/series/${series.id}`, data)
             toast({
                 title: 'Series settings saved',
                 status: 'success',
                 isClosable: true,
                 position: 'top',
             })
+            queryClient.setQueryData(['series', series.id], result.data)
             api.post(`/2/series/${series.id}/update`)
-        } catch(e) {
+        } catch (e) {
             setError(ErrorMessageFromResponse(e))
         }
     })
 
-    return <Box>
-        <form onSubmit={onSubmit}>
-            <Stack spacing="1rem">
-                {error && <Alert status="error" rounded="md">
-                    <AlertIcon />
-                    <AlertTitle>{error}</AlertTitle>    
-                </Alert>}
+    return <form onSubmit={onSubmit}>
+        <Flex wrap="wrap" gap="1rem">
+            {error && <Alert status="error" rounded="md">
+                <AlertIcon />
+                <AlertTitle>{error}</AlertTitle>
+            </Alert>}
 
-                <Heading fontSize="1.25rem">Externals</Heading>                        
+            <Flex direction="column" gap="0.5rem" basis="400px">
+                <Heading fontSize="1.25rem" fontWeight="600">Externals</Heading>
                 <FormControl>
                     <FormLabel>IMDb</FormLabel>
-                    <Input {...register('externals.imdb')}  type='text' />
+                    <Input {...register('externals.imdb')} type='text' />
                 </FormControl>
 
                 <FormControl>
                     <FormLabel>The Movie DB</FormLabel>
-                    <Input {...register('externals.themoviedb')}  type='text' />
+                    <Input {...register('externals.themoviedb')} type='text' />
                 </FormControl>
 
                 <FormControl>
                     <FormLabel>TVMaze</FormLabel>
-                    <Input {...register('externals.tvmaze')}  type='text' />
+                    <Input {...register('externals.tvmaze')} type='text' />
                 </FormControl>
 
                 <FormControl>
                     <FormLabel>TheTVDB</FormLabel>
-                    <Input {...register('externals.thetvdb', {})}  type='text' />
+                    <Input {...register('externals.thetvdb', {})} type='text' />
+                </FormControl>
+            </Flex>
+
+            <Flex direction="column" gap="0.5rem" basis="400px">
+                <Heading fontSize="1.25rem" fontWeight="600">Importers</Heading>
+                <FormControl>
+                    <FormLabel>Info</FormLabel>
+                    <Select {...register('importers.info')}>
+                        <option value="themoviedb">The Movie DB</option>
+                        <option value="thetvdb">TheTVDB</option>
+                        <option value="tvmaze">TVMaze</option>
+                    </Select>
                 </FormControl>
 
-                <Stack align="end">
-                    <Button type="submit" colorScheme="blue" isLoading={isSubmitting} loadingText='Saving'>Save</Button>
-                </Stack>
-            </Stack>
-        </form>
-    </Box>
+                <FormControl>
+                    <FormLabel>Episodes</FormLabel>
+                    <Select {...register('importers.episodes')}>
+                        <option value="themoviedb">The Movie DB</option>
+                        <option value="thetvdb">TheTVDB</option>
+                        <option value="tvmaze">TVMaze</option>
+                    </Select>
+                </FormControl>
+            </Flex>
+
+            <Flex align="end" justifyContent="end" gap="1rem" basis="100%">
+                <Button type="submit" colorScheme="blue" isLoading={isSubmitting} loadingText='Saving'>Save</Button>
+            </Flex>
+        </Flex>
+    </form>
 }
