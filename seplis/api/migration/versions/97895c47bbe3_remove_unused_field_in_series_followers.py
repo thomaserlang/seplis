eabd17dd5f200d7030cd0cc06e5e8919d22df156
Thomas Erlang <thomas@erlang.dk>
2023-03-04 17:33:26 +0100

    Migration for removing unused datetime field in series_followers

---
+++ seplis/api/migration/versions/97895c47bbe3_remove_unused_field_in_series_followers.py
@@ -0,0 +1,23 @@
+"""Remove unused field in series_followers
+
+Revision ID: 97895c47bbe3
+Revises: 5ddba6ae2808
+Create Date: 2023-03-04 17:01:27.824748
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '97895c47bbe3'
+down_revision = '5ddba6ae2808'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.drop_column('series_followers', 'datetime')
+    op.execute('update series_followers set created_at="2011-01-01 00:00:00" where isnull(created_at)')
+
+
+def downgrade():
+    pass
