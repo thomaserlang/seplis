6898c1f13971f1a672c5378dff00d5f2e3e3202d
Thomas Erlang <thomas@erlang.dk>
2023-03-04 17:34:11 +0100

    No need to handle the session manually here

--- seplis/api/routes/series_following.py
+++ seplis/api/routes/series_following.py
@@ -1,33 +1,29 @@
-from fastapi import APIRouter, Security, Depends
-from ..dependencies import authenticated, get_session, AsyncSession
+from fastapi import APIRouter, Security
+from ..dependencies import authenticated
 from .. import models, schemas, constants
 
 router = APIRouter(prefix='/2/series/{series_id}/following')
+
 
 @router.get('', response_model=schemas.Series_user_following)
 async def following_series(
     series_id: int,
     user: schemas.User_authenticated = Security(authenticated, scopes=[str(constants.LEVEL_USER)]),
-    session: AsyncSession = Depends(get_session),
 ):
-    return await models.Series_follower.get(series_id=series_id, user_id=user.id, session=session)
+    return await models.Series_follower.get(series_id=series_id, user_id=user.id)
     
 
 @router.put('', status_code=204)
 async def follow_series(
     series_id: int,    
     user: schemas.User_authenticated = Security(authenticated, scopes=[str(constants.LEVEL_USER)]),
-    session: AsyncSession = Depends(get_session),
 ):
-    await models.Series_follower.follow(series_id=series_id, user_id=user.id, session=session)
-    await session.commit()
+    await models.Series_follower.follow(series_id=series_id, user_id=user.id)
 
 
 @router.delete('', status_code=204)
 async def unfollow_series(
     series_id: int,    
     user: schemas.User_authenticated = Security(authenticated, scopes=[str(constants.LEVEL_USER)]),
-    session: AsyncSession = Depends(get_session),
 ):
-    await models.Series_follower.unfollow(series_id=series_id, user_id=user.id, session=session)
-    await session.commit()
+    await models.Series_follower.unfollow(series_id=series_id, user_id=user.id)
