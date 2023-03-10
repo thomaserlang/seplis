6cb5fed0b54e8a9c243234682a6a96d1d21e5ac5
Thomas Erlang <thomas@erlang.dk>
2023-03-10 19:03:06 +0100

    Fixed series recently aired not filtering as expected

--- seplis/api/routes/series_recently_aired.py
+++ seplis/api/routes/series_recently_aired.py
@@ -1,39 +1,34 @@
-from fastapi import APIRouter, Depends, Query, Security
+from fastapi import APIRouter, Depends
 import sqlalchemy as sa
 from datetime import datetime, timezone, timedelta
-from ..filter.series.user_following import filter_user_following_query
-from ..filter.series.user_can_watch import filter_user_can_watch_query
-from ..dependencies import authenticated, get_session, AsyncSession
-from .. import models, schemas, constants
+from ..filter.series import filter_series_query
+from ..filter.series.query_filter_schema import Series_query_filter
+from ..dependencies import get_session, AsyncSession
+from .. import models, schemas
 from ... import utils
 
 router = APIRouter(prefix='/2/series-recently-aired')
 
 @router.get('', response_model=schemas.Page_cursor_result[schemas.Series_and_episode])
 async def get_series_recently_aired(
-    user: schemas.User_authenticated = Security(authenticated, scopes=[str(constants.LEVEL_USER)]),
     session: AsyncSession=Depends(get_session),
-    page_query: schemas.Page_cursor_query = Depends(),
-    user_following: bool = Query(default=None),
-    user_can_watch: bool = Query(default=None),
+    page_cursor: schemas.Page_cursor_query = Depends(),
+    filter_query: Series_query_filter = Depends(),
 ):
     dt = datetime.now(tz=timezone.utc)
     episodes_query = sa.select(
         models.Episode.series_id,
         sa.func.min(models.Episode.number).label('episode_number'),
     ).where(
-        models.Series_follower.user_id == user.id,
-        models.Episode.series_id == models.Series_follower.series_id,
         models.Episode.air_datetime > (dt-timedelta(days=7)),
         models.Episode.air_datetime < dt,        
         models.Series.id == models.Episode.series_id,
     ).group_by(models.Episode.series_id)
     
-    if user_following != None:
-        episodes_query = filter_user_following_query(episodes_query, user_following=user_following, user_id=user.id)
-    if user_can_watch != None:
-        episodes_query = filter_user_can_watch_query(episodes_query, user_id=user.id, episode_number=models.Episode.number)
-    
+    episodes_query = filter_series_query(
+        query=episodes_query,
+        filter_query=filter_query,
+    )
     episodes_query = episodes_query.subquery()
 
     query = sa.select(models.Series, models.Episode).where(
@@ -45,6 +40,6 @@ async def get_series_recently_aired(
         models.Episode.series_id,
     )
 
-    p = await utils.sqlalchemy.paginate_cursor(session=session, query=query, page_query=page_query)
+    p = await utils.sqlalchemy.paginate_cursor(session=session, query=query, page_query=page_cursor)
     p.items = [schemas.Series_and_episode(series=item.Series, episode=item.Episode) for item in p.items]
     return p
