42c55c8120cf830dfd96ef5563bdcc8578e55fdf
Thomas Erlang <thomas@erlang.dk>
2023-03-19 22:45:38 +0100

    Run dev server

--- seplis/dev_server.py
+++ seplis/dev_server.py
@@ -1,23 +1,19 @@
-import os
 from subprocess import list2cmdline
 from honcho.manager import Manager
 
 def main():
-    src_path = os.path.dirname(os.path.realpath(__file__))
-
     start = [
-        ('api', ['python', 'runner.py', 'api'], src_path),
-        ('web', ['python', 'runner.py', 'web'], src_path),
-        ('worker', ['python', 'runner.py', 'worker'], src_path),
+        ('api', ['python', 'seplis/runner.py', 'api']),
+        ('web', ['python', 'seplis/runner.py', 'web']),
+        ('worker', ['python', 'seplis/runner.py', 'worker']),
     ]
 
     manager = Manager()
-    for name, cmd, cwd in start:
+    for name, cmd in start:
         manager.add_process(
             name, 
             list2cmdline(cmd),
             quiet=False, 
-            cwd=cwd,
         )
 
     manager.loop()
