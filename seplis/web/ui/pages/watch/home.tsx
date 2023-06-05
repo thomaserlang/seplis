08d4043eed68f2eb0b363f38a7a216774e698db9
Thomas Erlang <thomas@erlang.dk>
2023-06-05 16:46:27 +0200

    Changed title for watch home page

--- seplis/web/ui/pages/watch/home.tsx
+++ seplis/web/ui/pages/watch/home.tsx
@@ -16,7 +16,7 @@ export default function WatchHome() {
     const location = useLocation()
 
     useEffect(() => {
-        setTitle('Watch Home')
+        setTitle('Watch')
     }, [])
 
     useEffect(() => {
