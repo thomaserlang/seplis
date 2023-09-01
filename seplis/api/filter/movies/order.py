afbbafa592824526b331f7319b606153b12cfb81
Thomas Erlang <thomas@erlang.dk>
2023-09-01 22:15:27 +0200

    A bit better order by rating

--- seplis/api/filter/movies/order.py
+++ seplis/api/filter/movies/order.py
@@ -14,7 +14,7 @@ def order_query(query: any, filter_query: Movie_query_filter):
         elif sort.startswith('user_last_watched_at') and filter_query.user_has_watched != False:
             order.append(direction(models.Movie_watched.watched_at))
         elif sort.startswith('rating'):
-            order.append(direction(sa.func.coalesce(models.Movie.rating, -1)))
+            order.append(direction(sa.func.coalesce(models.Movie.rating, -1) * sa.func.coalesce(models.Movie.rating_votes, 0)))
         elif sort.startswith('popularity'):
             order.append(direction(models.Movie.popularity))
         elif sort.startswith('release_date'):
