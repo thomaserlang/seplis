7eff6d868107222bf2a6192de06aa1d615582e86
Thomas Erlang <thomas@erlang.dk>
2023-09-02 21:31:27 +0200

    Sort by release date

--- seplis/web/ui/components/movie/user-sort-select.tsx
+++ seplis/web/ui/components/movie/user-sort-select.tsx
@@ -4,15 +4,15 @@ import { forwardRef } from 'react'
 
 export const SelectMovieUserSort = forwardRef<any, SelectProps>((props, ref) => {
     return <Select ref={ref} {...props} placeholder="Sort">
-        <option value="user_watchlist_added_at_asc">Added to Watchlist at ASC</option>
+        <option value="popularity_desc">Popularity</option>
+        <option value="rating_desc">IMDb Rating</option>
+        <option value="release_date_desc">Release Date DESC</option>
+        <option value="release_date_asc">Release Date ASC</option>
         <option value="user_watchlist_added_at_desc">Added to Watchlist at DESC</option>
-        <option value="user_favorite_added_at_asc">Added to favorites at ASC</option>
+        <option value="user_watchlist_added_at_asc">Added to Watchlist at ASC</option>
         <option value="user_favorite_added_at_desc">Added to favorites at DESC</option>
-        <option value="user_last_watched_at_asc">Watched at ASC</option>
+        <option value="user_favorite_added_at_asc">Added to favorites at ASC</option>
         <option value="user_last_watched_at_desc">Watched at DESC</option>
-        <option value="rating_asc">Rating ASC</option>
-        <option value="rating_desc">Rating DESC</option>
-        <option value="popularity_asc">Popularity ASC</option>
-        <option value="popularity_desc">Popularity DESC</option>
+        <option value="user_last_watched_at_asc">Watched at ASC</option>
     </Select>
 })
