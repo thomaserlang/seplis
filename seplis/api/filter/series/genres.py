81c4cb82869df2884467e141be70e303b45e5213
Thomas Erlang <thomas@erlang.dk>
2025-08-07 23:10:15 +0200

    Possible fix for series genres returning incorrect results

--- seplis/api/filter/series/genres.py
+++ seplis/api/filter/series/genres.py
@@ -3,17 +3,12 @@ from .query_filter_schema import Series_query_filter
 from ... import models
 
 
-def filter_genres(query, filter_query: Series_query_filter):
+def filter_genres(
+    query: sa.orm.Query, filter_query: Series_query_filter
+) -> sa.orm.Query:
     if filter_query.genre_id:
-        genre_alias = sa.orm.aliased(models.Series_genre)
-        query = query.where(
-            sa.exists(
-                sa.select(1).where(
-                    genre_alias.series_id == models.Series.id,
-                    genre_alias.genre_id.in_(filter_query.genre_id),
-                )
-            )
-        )
+        # handled in order_query to prioritize series with most genre hits
+        pass
 
     if filter_query.not_genre_id:
         genre_alias = sa.orm.aliased(models.Series_genre)
