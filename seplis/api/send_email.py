2fae38867c4c2a9e67a26707ebd5d3afe13a6244
Thomas Erlang <thomas@erlang.dk>
2025-10-01 21:55:24 +0200

    Refactor email sending logic for improved readability and error handling

--- seplis/api/send_email.py
+++ seplis/api/send_email.py
@@ -1,24 +1,29 @@
+from email.header import Header
+from email.utils import formataddr, parseaddr
 import os
 import aiosmtplib
 import jinja2
 from email.mime.text import MIMEText
 from .. import config, logger
 
-template_loader = jinja2.FileSystemLoader(searchpath=os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'templates'))
+template_loader = jinja2.FileSystemLoader(
+    searchpath=os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates")
+)
 templateEnv = jinja2.Environment(
     loader=template_loader,
 )
 
+
 async def send_email(template_name: str, subject: str, to: str, **kwargs):
     if not config.data.smtp.from_email:
-        logger.warn('No smtp server has been configured')
+        logger.warning("No smtp server has been configured")
         return
     template = templateEnv.get_template(template_name)
-    message = MIMEText(template.render(kwargs), 'html')
-    message["From"] = config.data.smtp.from_email
-    message["To"] = to
-    message["Subject"] = subject
+    message = MIMEText(template.render(kwargs), "html")
+    display_name, address = parseaddr(config.data.smtp.from_email)
+    message["From"] = formataddr((str(Header(display_name, "utf-8")), address))
+    message["To"] = formataddr(("", to), "utf-8")
+    message["Subject"] = str(Header(subject, "utf-8"))
     await aiosmtplib.send(
         message,
         hostname=config.data.smtp.server,
@@ -30,18 +35,22 @@ async def send_email(template_name: str, subject: str, to: str, **kwargs):
 
 
 async def send_reset_password(to: str, url: str):
-    await send_email('email/reset_password.html', 'Reset password', to, url=url)
+    try:
+        await send_email("email/reset_password.html", "Reset password", to, url=url)
+    except Exception as e:
+        logger.exception(e)
+        raise
 
 
 async def send_password_changed(to: str):
     try:
-        await send_email('email/password_changed.html', 'Password changed', to)
+        await send_email("email/password_changed.html", "Password changed", to)
     except Exception as e:
         logger.exception(e)
 
 
 async def send_new_login(to: str, ip: str):
     try:
-        await send_email('email/new_login.html', 'New login', to, ip=ip)
+        await send_email("email/new_login.html", "New login", to, ip=ip)
     except Exception as e:
         logger.exception(e)
