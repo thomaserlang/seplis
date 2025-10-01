ed00aa56c982fcdfb7dc6ce4f13ebd8431a1ef8d
Thomas Erlang <thomas@erlang.dk>
2025-10-01 22:00:58 +0200

    Refactor reset password link generation for improved readability

--- seplis/api/models/reset_password.py
+++ seplis/api/models/reset_password.py
@@ -1,3 +1,4 @@
+from urllib.parse import urljoin
 import uuid
 import sqlalchemy as sa
 
@@ -7,8 +8,9 @@ from ..database import database
 from ... import config
 from datetime import datetime, timedelta, timezone
 
+
 class Reset_password(Base):
-    __tablename__ = 'reset_password'
+    __tablename__ = "reset_password"
 
     user_id = sa.Column(sa.Integer, primary_key=True, autoincrement=False)
     key = sa.Column(sa.String, primary_key=True)
@@ -18,10 +20,12 @@ class Reset_password(Base):
     async def create_reset_link(user_id: int) -> str:
         async with database.session() as session:
             key = str(uuid.uuid4())
-            await session.execute(sa.insert(Reset_password).values(
-                user_id=user_id,
-                key=key,
-                expires=datetime.now(tz=timezone.utc) + timedelta(minutes=30),
-            ))
+            await session.execute(
+                sa.insert(Reset_password).values(
+                    user_id=user_id,
+                    key=key,
+                    expires=datetime.now(tz=timezone.utc) + timedelta(minutes=30),
+                )
+            )
             await session.commit()
-            return config.data.web.url + f'/users/reset-password/{key}'
+            return urljoin(str(config.data.web.url), f"/users/reset-password/{key}")
