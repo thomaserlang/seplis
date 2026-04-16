05d2b8744a9a7636e4d07e6c58ac72819791c1c4
Thomas Erlang <thomas@erlang.dk>
2026-04-17 00:41:18 +0200

    refactor: remove MainShell wrapper from home page component

--- seplis-ui/src/features/home/home.page.tsx
+++ seplis-ui/src/features/home/home.page.tsx
@@ -1,8 +1,7 @@
-import { MainShell } from '@/components/main-shell'
 import { Box } from '@mantine/core'
 import { useActiveUser } from '../user/api/active-user.api'
-import { LandingView } from './components/landing-view'
 import { HomeView } from './components/home-view'
+import { LandingView } from './components/landing-view'
 
 export function Component() {
     const [user] = useActiveUser()
@@ -10,10 +9,8 @@ export function Component() {
     if (!user) return <LandingView />
 
     return (
-        <MainShell>
-            <Box mt="-0.25rem">
-                <HomeView />
-            </Box>
-        </MainShell>
+        <Box mt="-0.25rem">
+            <HomeView />
+        </Box>
     )
 }
